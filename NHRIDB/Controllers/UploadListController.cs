bbb29e49a8c98d9c93f34140906fdb7304d7489f
jacky <jacky@e-tec.com.tw>
2023-04-11 09:22:29 +0800

    增加當月已/未上傳之機構

---
+++ NHRIDB/Controllers/UploadListController.cs
@@ -0,0 +1,51 @@
+﻿using NHRIDB.Filter;
+using NHRIDB.Models.ViewModels;
+using NHRIDB_DAL.DAL;
+using NHRIDB_DAL.DbModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NHRIDB.Controllers
+{
+    public class UploadListController : BasicController
+    {
+        private SysLogDA _SysLogDA;
+        private HospitalDA _hospitalDA;
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            _hospitalDA = new HospitalDA(_db);
+            _SysLogDA = new SysLogDA(_db);
+        }
+        // GET: UploadList
+        public ActionResult Index()
+        {
+            HospitalUploadViewModel model = new HospitalUploadViewModel();
+            model.items =
+                _db.Hospital
+                    .Select(e => new HospitalUploadlist
+                    {
+                        HospitalId = e.id,
+                        name_tw = e.name_tw,
+                        name_en = e.name_en,
+                        Count = _db.TubeData
+                                .Where(f => f.hospitalId.Equals(e.id)).Count(),
+                        HasRow = _db.TubeData
+                                .Where(f => f.hospitalId.Equals(e.id)).Count() > 0 ? true : false,
+                        LastDate = _db.TubeData
+                                .Where(f => f.hospitalId.Equals(e.id))
+                                .OrderByDescending(f => f.createDate)
+                                .Select(f => f.createDate)
+                                .FirstOrDefault()
+                    })
+                    .OrderByDescending(e => e.LastDate)
+                    .ToList();
+            ViewData.Model = model;
+            return View();
+        }
+    }
+}
