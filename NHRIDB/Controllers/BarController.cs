80ebd8b5ba5f820baaace59b84f2212d8b7f4ce5
jacky <jacky@e-tec.com.tw>
2023-04-28 17:21:44 +0800

    增加總結數目查詢條件

--- NHRIDB/Controllers/BarController.cs
+++ NHRIDB/Controllers/BarController.cs
@@ -12,33 +12,144 @@ using System.Web.Mvc;
 namespace NHRIDB.Controllers
 {
     public class BarController : BasicController
-    {       
+    {
         private HospitalDA _hospitalDA;
         private TubeDataTotalDA _tubeTotal;
 
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             base.OnActionExecuting(filterContext);
-          
+
             _hospitalDA = new HospitalDA(_db);
-            _tubeTotal= new TubeDataTotalDA();
+            _tubeTotal = new TubeDataTotalDA();
         }
 
         [MvcAdminRightAuthorizeFilter(param = 'r')]
         // GET: Bar
-        public ActionResult Index(Nullable<Guid> hosId=null)
+        public ActionResult Index(Nullable<Guid> hosId = null)
         {
             BarViewModel model = new BarViewModel();
-     
+
+            model.conditions.hospitalId = hosId;
+            model.conditions.hosId = Guid.NewGuid();
+            //model.hosId = hosId;
+            model.conditions.caseTimes = 1;
             model.leapProject = _leapProject;
             model.selfHos = _hospitalDA.GetHospital(_hos);
             model.hospitalSelect = new SelectList(_hospitalDA.GetQuery().ToList(), "id", "name_tw");
-            if (!_leapProject)
+            model.genderSelect = new List<SelectListItem>
             {
-                hosId = _hos;
+                new SelectListItem { Text = "男" , Value = "M" },
+                new SelectListItem { Text = "女" , Value = "F" }
+            };
+            model.ageSelect = new List<SelectListItem>();
+            for (int i = 1; i <= 100; i++)
+            {
+                model.ageSelect.Add(new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString()
+                });
             }
+            model.endYearSelect = new List<SelectListItem>();
+            for (int i = 2002; i <= DateTime.Now.Year; i++)
+            {
+                model.endYearSelect.Add(new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString(),
+                });
+            }
+            model.caseTimesSelect = new List<SelectListItem>();
+            for (int i = 1; i <= 5; i++)
+            {
+                model.caseTimesSelect.Add(new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString()
+                });
+            }
+            model.specimenSelect = new List<SelectListItem> {
+                new SelectListItem { Text = "血液" , Value = "blood" , Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("blood") >= 0?true:false},
+                new SelectListItem { Text = "冷凍組織" , Value = "frozenTissue", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("frozenTissue") >= 0?true:false},
+                new SelectListItem { Text = "石蠟切片" , Value = "paraffinSection", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("paraffinSection") >= 0?true:false},
+                new SelectListItem { Text = "尿液" , Value = "waxBlock", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("waxBlock") >= 0?true:false},
+                new SelectListItem { Text = "胸水" , Value = "pleuraleffusion", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("pleuraleffusion") >= 0?true:false},
+                new SelectListItem { Text = "腹水" , Value = "ascites", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("ascites") >= 0?true:false},
+                new SelectListItem { Text = "骨髓液" ,Value = "boneMarrow", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("boneMarrow") >= 0?true:false},
+                new SelectListItem { Text = "腦脊髓液" , Value = "CSF", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("CSF") >= 0?true:false}
+            };
+            model.datas = _tubeTotal.GetTotal(model.conditions);
+            model.columns = _tubeTotal.GetColummns();
 
-            model.datas = _tubeTotal.GetTotal(hosId);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(BarViewModel model)
+        {
+            model.selfHos = _hospitalDA.GetHospital(_hos);
+            model.hospitalSelect = new SelectList(_hospitalDA.GetQuery().ToList(), "id", "name_tw");
+            model.genderSelect = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "男" , Value = "M" },
+                new SelectListItem { Text = "女" , Value = "F" }
+            };
+            model.ageSelect = new List<SelectListItem>();
+            for (int i = 1; i <= 100; i++)
+            {
+                model.ageSelect.Add(new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString()
+                });
+            }
+            model.endYearSelect = new List<SelectListItem>();
+            for (int i = 2002; i <= DateTime.Now.Year; i++)
+            {
+                model.endYearSelect.Add(new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString()
+                });
+            }
+            model.caseTimesSelect = new List<SelectListItem>();
+            for (int i = 1; i < 6; i++)
+            {
+                model.caseTimesSelect.Add(new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString()
+                });
+            }
+            model.specimenSelect = new List<SelectListItem> {
+                new SelectListItem { Text = "血液" , Value = "blood" , Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("blood") >= 0?true:false},
+                new SelectListItem { Text = "冷凍組織" , Value = "frozenTissue", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("frozenTissue") >= 0?true:false},
+                new SelectListItem { Text = "石蠟切片" , Value = "paraffinSection", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("paraffinSection") >= 0?true:false},
+                new SelectListItem { Text = "尿液" , Value = "waxBlock", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("waxBlock") >= 0?true:false},
+                new SelectListItem { Text = "胸水" , Value = "pleuraleffusion", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("pleuraleffusion") >= 0?true:false},
+                new SelectListItem { Text = "腹水" , Value = "ascites", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("ascites") >= 0?true:false},
+                new SelectListItem { Text = "骨髓液" ,Value = "boneMarrow", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("boneMarrow") >= 0?true:false},
+                new SelectListItem { Text = "腦脊髓液" , Value = "CSF", Selected = Request.Form["specimen"] == null? false: Request.Form["specimen"].IndexOf("CSF") >= 0?true:false}
+                };
+            model.conditions.blood = model.specimenSelect.Where(e => e.Value.Equals("blood")).SingleOrDefault().Selected;
+            model.conditions.frozenTissue = model.specimenSelect.Where(e => e.Value.Equals("frozenTissue")).SingleOrDefault().Selected;
+            model.conditions.paraffinSection = model.specimenSelect.Where(e => e.Value.Equals("paraffinSection")).SingleOrDefault().Selected;
+            model.conditions.waxBlock = model.specimenSelect.Where(e => e.Value.Equals("waxBlock")).SingleOrDefault().Selected;
+            model.conditions.pleuraleffusion = model.specimenSelect.Where(e => e.Value.Equals("pleuraleffusion")).SingleOrDefault().Selected;
+            model.conditions.ascites = model.specimenSelect.Where(e => e.Value.Equals("ascites")).SingleOrDefault().Selected;
+            model.conditions.boneMarrow = model.specimenSelect.Where(e => e.Value.Equals("boneMarrow")).SingleOrDefault().Selected;
+            model.conditions.CSF = model.specimenSelect.Where(e => e.Value.Equals("CSF")).SingleOrDefault().Selected;
+
+            if (Request.Form["hosId"] != null && !string.IsNullOrEmpty(Request.Form["hosId"]))
+            {
+                model.conditions.hospitalId = Guid.Parse(Request.Form["hosId"]);
+            }
+            //else
+            //{
+            //    model.conditions.hospitalId = _hos;
+            //}
+            model.datas = _tubeTotal.GetTotal(model.conditions);
+
             model.columns = _tubeTotal.GetColummns();
 
             return View(model);
--- NHRIDB_DAL/DAL/TubeDataTotalDA.cs
+++ NHRIDB_DAL/DAL/TubeDataTotalDA.cs
@@ -10,9 +10,9 @@ using System.Threading.Tasks;
 
 namespace NHRIDB_DAL.DAL
 {
-  public  class TubeDataTotalDA : DataAccess
+    public class TubeDataTotalDA : DataAccess
     {
-      
+
         public List<InfoColummns> GetColummns()
         {
             return TypeDescriptor.GetProperties(typeof(TubeDataTotalType))
@@ -27,21 +27,176 @@ namespace NHRIDB_DAL.DAL
               .ToList();
         }
 
-        public List<GetTotal_Result> GetTotal(Nullable<Guid> hospitalId) {
-            if (hospitalId.HasValue)
+        public List<GetTotal_Result> GetTotal(BarTubeDataType model)
+        {
+            if (!model.hosId.HasValue)
             {
-                return _db.GetTotal(hospitalId.Value.ToString()).ToList();
+                var para = _db.GetTotal(
+                    model.hosId?.ToString(),
+                    model.hospitalId?.ToString(),
+                    model.regionKey,
+                    model.diagnosisKey,
+                    model.endYearSt,
+                    model.endYearEd,
+                    model.ageSt?.ToString(),
+                    model.ageEt?.ToString(),
+                    model.gender,
+                    model.blood?model.blood:(bool?)null,
+                    model.frozenTissue? model.frozenTissue:(bool?)null,
+                    model.paraffinSection? model.paraffinSection:(bool?) null,
+                    model.urine? model.urine :(bool?) null,
+                    model.paraffinSection? model.paraffinSection :(bool?) null,
+                    model.ascites? model.ascites :(bool?) null,
+                    model.boneMarrow? model.boneMarrow :(bool?) null,
+                    model.CSF? model.CSF :(bool?) null,
+                    model.caseTimes
+                    )
+                    .Select(e => new GetTotal_Result
+                    {
+                        rId = e.rId,
+                        dId = e.dId,
+                        rName = e.rName,
+                        dName = e.dName,
+                        sum_blood = e.sum_blood,
+                        sum_frozenTissue = e.sum_frozenTissue,
+                        sum_waxBlock = e.sum_waxBlock,
+                        sum_urine = e.sum_urine,
+                        sum_urineClearLiquid = e.sum_urineClearLiquid,
+                        sum_urinePellet = e.sum_urinePellet,
+                        sum_tissueDNA = e.sum_tissueDNA,
+                        sum_tissueRNA = e.sum_tissueRNA,
+                        sum_stool = e.sum_stool,
+                        sum_stoolDNA = e.sum_stoolDNA,
+                        sum_sampleless = e.sum_sampleless,
+                        sum_pleuraleffusion = e.sum_pleuraleffusion,
+                        sum_CSF = e.sum_CSF,
+                        sum_ascites = e.sum_ascites,
+                        sum_boneMarrow = e.sum_boneMarrow,
+                        sum_serum = e.sum_serum,
+                        sum_plasma = e.sum_plasma,
+                        sum_buffyCoat = e.sum_buffyCoat,
+                        sum_wholeBlood = e.sum_wholeBlood,
+                        sum_paraffinSection = e.sum_paraffinSection,
+                        sum_bile = e.sum_bile,
+                        sum_hair = e.sum_hair,
+                        sum_saliva = e.sum_saliva,
+                        sum_bloodDNA = e.sum_bloodDNA,
+                        sum_CDM = e.sum_CDM,
+                        sum_questionnaire = e.sum_questionnaire,
+                        sum_CT = e.sum_CT,
+                        sum_MRI = e.sum_MRI,
+                        sum_ultrasound = e.sum_ultrasound,
+                        sum_digit_pathology_image_data = e.sum_digit_pathology_image_data,
+                        sum_DNA_WGS = e.sum_DNA_WGS,
+                        sum_DNA_WES = e.sum_DNA_WES,
+                        sum_DNA_pannel = e.sum_DNA_pannel,
+                        sum_RNA = e.sum_RNA
+                    })
+                    .ToList();
+                return para;
             }
-            else {
-                return _db.GetTotal("").ToList();
+            else
+            {
+                var para =
+                _db.GetTotal(
+                    model.hosId?.ToString(),
+                    model.hospitalId?.ToString(),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    model.caseTimes
+                    )
+                    .Select(e => new GetTotal_Result
+                    {
+                        rId = e.rId,
+                        dId = e.dId,
+                        rName = e.rName,
+                        dName = e.dName,
+                        sum_blood = e.sum_blood,
+                        sum_frozenTissue = e.sum_frozenTissue,
+                        sum_waxBlock = e.sum_waxBlock,
+                        sum_urine = e.sum_urine,
+                        sum_urineClearLiquid = e.sum_urineClearLiquid,
+                        sum_urinePellet = e.sum_urinePellet,
+                        sum_tissueDNA = e.sum_tissueDNA,
+                        sum_tissueRNA = e.sum_tissueRNA,
+                        sum_stool = e.sum_stool,
+                        sum_stoolDNA = e.sum_stoolDNA,
+                        sum_sampleless = e.sum_sampleless,
+                        sum_pleuraleffusion = e.sum_pleuraleffusion,
+                        sum_CSF = e.sum_CSF,
+                        sum_ascites = e.sum_ascites,
+                        sum_boneMarrow = e.sum_boneMarrow,
+                        sum_serum = e.sum_serum,
+                        sum_plasma = e.sum_plasma,
+                        sum_buffyCoat = e.sum_buffyCoat,
+                        sum_wholeBlood = e.sum_wholeBlood,
+                        sum_paraffinSection = e.sum_paraffinSection,
+                        sum_bile = e.sum_bile,
+                        sum_hair = e.sum_hair,
+                        sum_saliva = e.sum_saliva,
+                        sum_bloodDNA = e.sum_bloodDNA,
+                        sum_CDM = e.sum_CDM,
+                        sum_questionnaire = e.sum_questionnaire,
+                        sum_CT = e.sum_CT,
+                        sum_MRI = e.sum_MRI,
+                        sum_ultrasound = e.sum_ultrasound,
+                        sum_digit_pathology_image_data = e.sum_digit_pathology_image_data,
+                        sum_DNA_WGS = e.sum_DNA_WGS,
+                        sum_DNA_WES = e.sum_DNA_WES,
+                        sum_DNA_pannel = e.sum_DNA_pannel,
+                        sum_RNA = e.sum_RNA
+                    })
+                    .ToList();
+                return para;                
+                //.ToList();
             }
-          
+
         }
 
         public List<GetDifferentTotal_Result> GetDifferent(Guid hospitalId)
-        {       
-                return _db.GetDifferentTotal(hospitalId).ToList();
+        {
+            return _db.GetDifferentTotal(hospitalId).ToList();
         }
+
+    }
+    public class BarTubeDataType : TubeDataType
+    {
+        [DisplayName("機構代號")]
+        public Nullable<Guid> hospitalId { get; set; }
+
+        public Nullable<Guid> hosId { get; set; }
+
+        [DisplayName("收案年份 (西元年)起")]
+        [Required]
+        public string endYearSt { get; set; }
+
+        [DisplayName("收案年份 (西元年)迄")]
+        [Required]
+        public string endYearEd { get; set; }
+
+        [DisplayName("收案時年齡 (歲)起")]
+        [Required]
+        public string ageSt { get; set; }
+
+        [DisplayName("收案時年齡 (歲)迄")]
+        [Required]
+        public string ageEt { get; set; }
+        [DisplayName("收案次數")]
+        [Required]
+        public int caseTimes { get; set; }
 
     }
 }
